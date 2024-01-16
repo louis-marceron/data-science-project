8f590b19053a6f5c3213820b4031942f978344a1
Louis Marceron <louis.marceron@protonmail.com>
2024-01-16 12:08:41 +0100

    feat: clean lieux

---
+++ src/cleanLieux.py
@@ -0,0 +1,153 @@
+import csv
+
+import pandas as pd
+import utils
+
+ATTRIBUTES_TO_DROP = [
+    'Numéro_Route',
+    'Indice_Numérique_Route',
+    'Lettre_Indice_Route',
+    'Numéro_PR',
+    'Distance_PR',
+    'Largeur_Terre_Plein_Central'
+]
+
+# Define mappings for the LIEUX attributes
+catr_mapping = {
+    1: "Autoroute",
+    2: "Route nationale",
+    3: "Route Départementale",
+    4: "Voie Communale",
+    5: "Hors réseau public",
+    6: "Parc de stationnement ouvert à la circulation publique",
+    7: "Route de métropole urbaine",
+    9: "Autre"
+}
+
+circ_mapping = {
+    -1: "Non renseigné",
+    1: "A sens unique",
+    2: "Bidirectionnelle",
+    3: "A chaussées séparées",
+    4: "Avec voies d’affectation variable"
+}
+
+vosp_mapping = {
+    -1: "Non renseigné",
+    0: "Sans objet",
+    1: "Piste cyclable",
+    2: "Bande cyclable",
+    3: "Voie réservée"
+}
+
+prof_mapping = {
+    -1: "Non renseigné",
+    1: "Plat",
+    2: "Pente",
+    3: "Sommet de côte",
+    4: "Bas de côte"
+}
+
+plan_mapping = {
+    -1: "Non renseigné",
+    1: "Partie rectiligne",
+    2: "En courbe à gauche",
+    3: "En courbe à droite",
+    4: "En « S »"
+}
+
+surf_mapping = {
+    -1: "Non renseigné",
+    1: "Normale",
+    2: "Mouillée",
+    3: "Flaques",
+    4: "Inondée",
+    5: "Enneigée",
+    6: "Boue",
+    7: "Verglacée",
+    8: "Corps gras – huile",
+    9: "Autre"
+}
+
+infra_mapping = {
+    -1: "Non renseigné",
+    0: "Aucun",
+    1: "Souterrain - tunnel",
+    2: "Pont - autopont",
+    3: "Bretelle d’échangeur ou de raccordement",
+    4: "Voie ferrée",
+    5: "Carrefour aménagé",
+    6: "Zone piétonne",
+    7: "Zone de péage",
+    8: "Chantier",
+    9: "Autres"
+}
+
+situ_mapping = {
+    -1: "Non renseigné",
+    0: "Aucun",
+    1: "Sur chaussée",
+    2: "Sur bande d’arrêt d’urgence",
+    3: "Sur accotement",
+    4: "Sur trottoir",
+    5: "Sur piste cyclable",
+    6: "Sur autre voie spéciale",
+    8: "Autres"
+}
+
+columns_mapping = {
+    "Num_Acc": "Identifiant_Accident",
+    "catr": "Catégorie_Route",
+    "voie": "Numéro_Route",
+    "v1": "Indice_Numérique_Route",
+    "v2": "Lettre_Indice_Route",
+    "circ": "Régime_Circulation",
+    "nbv": "Nombre_Voies",
+    "vosp": "Voie_Réservée",
+    "prof": "Profil_Long",
+    "pr": "Numéro_PR",
+    "pr1": "Distance_PR",
+    "plan": "Tracé_Plan",
+    "lartpc": "Largeur_Terre_Plein_Central",
+    "larrout": "Largeur_Chaussée",
+    "surf": "Etat_Surface",
+    "infra": "Aménagement_Infrastructure",
+    "situ": "Situation_Accident",
+    "vma": "Vitesse_Maximale_Autorisée",
+}
+
+
+def cleanLieux(input_path, output_path):
+    # Read the CSV file
+    lieux = pd.read_csv(input_path, delimiter=';')
+
+    # Convert 'Num_Acc' to string
+    lieux['Num_Acc'] = lieux['Num_Acc'].astype(str)
+
+    lieux['voie'] = lieux['voie'].fillna("Non renseigné")
+    lieux['v2'] = lieux['v2'].fillna("Non renseigné")
+
+    # Handle 'lartpc', convert empty strings to -1
+    lieux['lartpc'] = pd.to_numeric(lieux['lartpc'], errors='coerce').fillna(-1)
+
+    # Remove white spaces in 'larrout'
+    lieux['larrout'] = lieux['larrout'].str.strip()
+
+    # Apply mappings
+    lieux["catr"].replace(catr_mapping, inplace=True)
+    lieux["circ"].replace(circ_mapping, inplace=True)
+    lieux["vosp"].replace(vosp_mapping, inplace=True)
+    lieux["prof"].replace(prof_mapping, inplace=True)
+    lieux["plan"].replace(plan_mapping, inplace=True)
+    lieux["surf"].replace(surf_mapping, inplace=True)
+    lieux["infra"].replace(infra_mapping, inplace=True)
+    lieux["situ"].replace(situ_mapping, inplace=True)
+
+    # Rename columns as needed
+    lieux.rename(columns=columns_mapping, inplace=True)
+
+    # Drop specified attributes
+    lieux.drop(columns=ATTRIBUTES_TO_DROP, inplace=True)
+
+    lieux.to_csv(output_path, index=False, sep=';', quoting=csv.QUOTE_NONNUMERIC)
+    utils.validate_csv(output_path, delimiter=';')
--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import os
 from TextData import TextData
 from cleanUsagers import cleanUsagers
 from cleanVehicules import cleanVehicules
+from cleanLieux import cleanLieux
 
 ANNEE = 2022
 
@@ -21,15 +22,7 @@ def getPathVehicules(annee):
     return "data/" + str(annee) + "/vehicules-" + str(annee) + ".csv"
 
 
-# Attributes to drop
-ATTRIBUTES_LIEUX = [
-    'voie',
-    'v1',
-    'v2',
-    'pr',
-    'pr1',
-    'lartpc'
-]
+
 ATTRIBUTES_CARACTERISTIQUES = [
     'com',
     'adr'
@@ -48,26 +41,10 @@ if __name__ == '__main__':
 
     cleanUsagers(getPathUsagers(ANNEE), output_dir_path + os.path.basename(getPathUsagers(ANNEE)))
     cleanVehicules(getPathVehicules(ANNEE), output_dir_path + os.path.basename(getPathVehicules(ANNEE)))
-
-    # Clean lieux 2022
-    lieux = TextData(getPathLieux(ANNEE))
-    lieux.read_csv() \
-        .drop_attributes(ATTRIBUTES_LIEUX) \
-        .output_csv(output_dir_path + os.path.basename(getPathLieux(ANNEE)))
+    cleanLieux(getPathLieux(ANNEE), output_dir_path + os.path.basename(getPathLieux(ANNEE)))
 
     # Clean caracteristiques 2022
     caracteristiques = TextData(getPathCaracteristiques(ANNEE))
     caracteristiques.read_csv() \
         .drop_attributes(ATTRIBUTES_CARACTERISTIQUES) \
         .output_csv(output_dir_path + os.path.basename(getPathCaracteristiques(ANNEE)))
-
-    # # Clean vehicules 2022
-    # values_to_replace = {value: "99" for value in ["1", "4", "5", "6", "8", "9", "11", "12",
-    #                                                "16", "17", "18", "19", "20", "21", "39",
-    #                                                "40", "41", "42", "43", "50", "60", "80"]}
-    # vehicules = TextData(getPathVehicules(ANNEE))
-    # vehicules.read_csv() \
-    #     .drop_attributes(ATTRIBUTES_VEHICULES) \
-    #     .convert_columns_to_string(["catv"]) \
-    #     .replace_column_values("catv", values_to_replace) \
-    #     .output_csv(output_dir_path + os.path.basename(getPathVehicules(ANNEE)))
